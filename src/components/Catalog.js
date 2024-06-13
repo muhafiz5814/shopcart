2c12cacd26c33ad903a085580665ce85ca6030dc
Muhafiz Raza <muhafizraza1209@gmail.com>
2024-06-13 17:00:12 +0530

    Connect the connect redux store in Catalog Component using connect from react-redux

--- src/components/Catalog.js
+++ src/components/Catalog.js
@@ -1,25 +1,44 @@
 import React from "react";
 import Product from "./Product";
+import {connect} from "react-redux"
+import { setFilter, clearFilter } from "../store/actions/catalogActions";
 
-const Catalog = () => {
+const Catalog = ({departments, products, onSetFilter, onClearFilter}) => {
   return (
     <div className="catalog">
       <h1>Catalog</h1>
-      <button className="department-filters" onClick={() => {}}>
-        Electronics
-      </button>
-      <button className="department-filters clear-btn" onClick={() => {}}>
+      {departments && 
+        departments.map(department => (
+          <button key={department.id} className="department-filters" onClick={() => onSetFilter(department.title)}>
+            {department.title}
+          </button>
+        ))
+      }
+      <button className="department-filters clear-btn" onClick={() => onClearFilter()}>
         Clear Filter
       </button>
       <div className="product-units">
-        <Product
-          product="Fancy Robotic Sushi Maker"
-          cost={200}
-          department="Electronics"
-        />
+        {products && products.map(product => <Product key={product.id} {...product} />)}
       </div>
     </div>
   );
 };
 
-export default Catalog;
+const filterProducts = (products, filter) => filter ? products.filter(product => product.department === filter) : products
+
+const mapStateToProps = (state) => {
+  return {
+    departments: state.catalog.departments,
+    products: filterProducts(state.catalog.products, state.catalog.filter)
+  }
+}
+
+const mapDispathToProps = dispatch => {
+  return {
+    onSetFilter: departnent => dispatch(setFilter(departnent)),
+    onClearFilter: () => dispatch(clearFilter())
+  }
+}
+
+export default connect(mapStateToProps, mapDispathToProps)(Catalog)
+
