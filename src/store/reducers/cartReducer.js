c637bcdfd7bf84faf30b52e0ca8e8347343168e8
Muhafiz Raza <muhafizraza1209@gmail.com>
2024-06-13 18:53:13 +0530

    build Cart Actions and update Reducer

--- src/store/reducers/cartReducer.js
+++ src/store/reducers/cartReducer.js
@@ -5,5 +5,45 @@ const initialState = {
 }
 
 export default (state = initialState, action) => {
-  return state
+  switch (action.type) {
+    case "ADD_TO_CART": {
+      return state.items.find(item => item.code === action.product.code) ? {
+        totalProducts: state.totalProducts + 1,
+        totalCost: state.totalCost + action.product.cost,
+        items: state.items.map(item => item.code === action.product.code ? {...item, quantiy: item.quantity + 1} : item)
+      } : {
+        totalProducts: state.totalProducts + 1,
+        totalCost: state.totalCost + action.product.cost,
+        items: [...state.items, {...action.product, quantity: 1}]
+      }
+    }
+    
+    case "SET_QUANTITY": {
+      let revisedItems = state.items.map(item => item.code === action.code ? {...item, quantiy: action.quantity} : item)
+
+      let revisedTotalProducts = revisedItems.reduce((prev, curr) => prev + curr.quantity, 0)
+      
+      let revisedTotalCost = revisedItems.reduce((prev, curr) => prev + curr.quantity * curr.cost, 0)
+
+      return {
+        totalProducts: revisedTotalProducts,
+        totalCost: revisedTotalCost,
+        items: revisedItems
+      }
+    }
+
+    case "REMOVE_ITEM": {
+      let findItem = state.items.find(item => item.code === action.code)
+
+      return findItem ? {
+        totalProducts: state.totalProducts - findItem.quantity,
+        totalCost: state.totalCost - findItem.quantity * findItem.cost,
+        items: state.items.filter(item => item.code !== action.code)
+      } : state
+    }
+
+    default:{
+      return state
+    }
+  }
 }
