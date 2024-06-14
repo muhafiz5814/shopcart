4b63937788eee97d4ea631d3ab3bcd16d50d74f8
Muhafiz Raza <muhafizraza1209@gmail.com>
2024-06-14 12:15:54 +0530

    Dispatch Cart actions in Cart component

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,18 +1,23 @@
 import React from "react";
 import CartItem from "./CartItem";
 import CartTotals from "./CartTotals";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { setQuantity, removeItem } from "../store/actions/cartActions";
 
 const Cart = () => {
 
   const {totalProducts, totalCost, items} = useSelector(state => state.cart)
-
+  const dispatch = useDispatch()
   return (
     <div className="cart">
       <h1>Cart</h1>
       <div className="cart-contents">
         {items && 
-          items.map(item => <CartItem key={item.id} {...item} />)
+          items.map(item => <CartItem key={item.id} {...item} onQuantityChange={(qty) => 
+              qty > 0 ? dispatch(setQuantity(item.code, qty)) : dispatch(removeItem(item.code))
+            }
+            onItemRemove={() => dispatch(removeItem(item.code))}
+          />)
         }
       </div>
       <CartTotals totalProducts={totalProducts} totalCost={totalCost} />
