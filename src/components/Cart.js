932e76da746de2dabce6b784634475500f692dc6
Muhafiz Raza <muhafizraza1209@gmail.com>
2024-06-14 12:23:10 +0530

    Optimise component to skip re-rendering if props are unchanged or parent component has re-rendered

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,28 +1,28 @@
-import React from "react";
+import React, { memo } from "react";
 import CartItem from "./CartItem";
 import CartTotals from "./CartTotals";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector, useDispatch, shallowEqual } from "react-redux";
 import { setQuantity, removeItem } from "../store/actions/cartActions";
 
-const Cart = () => {
-
-  const {totalProducts, totalCost, items} = useSelector(state => state.cart)
-  const dispatch = useDispatch()
-  return (
-    <div className="cart">
-      <h1>Cart</h1>
-      <div className="cart-contents">
-        {items && 
-          items.map(item => <CartItem key={item.id} {...item} onQuantityChange={(qty) => 
-              qty > 0 ? dispatch(setQuantity(item.code, qty)) : dispatch(removeItem(item.code))
-            }
-            onItemRemove={() => dispatch(removeItem(item.code))}
-          />)
-        }
+const Cart = memo(() => {
+    const {totalProducts, totalCost, items} = useSelector(state => state.cart, shallowEqual)
+    const dispatch = useDispatch()
+    return (
+      <div className="cart">
+        <h1>Cart</h1>
+        <div className="cart-contents">
+          {items && 
+            items.map(item => <CartItem key={item.id} {...item} onQuantityChange={(qty) => 
+                qty > 0 ? dispatch(setQuantity(item.code, qty)) : dispatch(removeItem(item.code))
+              }
+              onItemRemove={() => dispatch(removeItem(item.code))}
+            />)
+          }
+        </div>
+        <CartTotals totalProducts={totalProducts} totalCost={totalCost} />
       </div>
-      <CartTotals totalProducts={totalProducts} totalCost={totalCost} />
-    </div>
-  );
-};
+    );
+  }
+)
 
 export default Cart;
