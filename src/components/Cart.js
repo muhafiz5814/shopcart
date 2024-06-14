0b2fd89616cb0fe139b5f5b6cb5acf999335aefd
Muhafiz Raza <muhafizraza1209@gmail.com>
2024-06-14 11:35:19 +0530

    fetch states in Cart component and show on UI

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,15 +1,21 @@
 import React from "react";
 import CartItem from "./CartItem";
 import CartTotals from "./CartTotals";
+import { useSelector } from "react-redux";
 
 const Cart = () => {
+
+  const {totalProducts, totalCost, items} = useSelector(state => state.cart)
+
   return (
     <div className="cart">
       <h1>Cart</h1>
       <div className="cart-contents">
-        <CartItem product="Fancy Robotic Sushi Maker" cost={200} quantity={2} />
+        {items && 
+          items.map(item => <CartItem key={item.id} {...item} />)
+        }
       </div>
-      <CartTotals totalProducts={2} totalCost={400} />
+      <CartTotals totalProducts={totalProducts} totalCost={totalCost} />
     </div>
   );
 };
