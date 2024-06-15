b94918a65230c3aefe3022d0fe8af37a80de4227
Muhafiz Raza <muhafizraza1209@gmail.com>
2024-06-15 10:52:19 +0530

    Create the initCatalog Action and handle it in Reducer using thunk function

--- src/store/actions/catalogActions.js
+++ src/store/actions/catalogActions.js
@@ -1,3 +1,26 @@
+import { getDepartments, getProducts } from "../../services/catalog"
+
+// This is a thunk function
+export const initCatalog = () => async dispatch => {
+  // fetch data from external service
+  try {
+    const data = await Promise.all([getDepartments(), getProducts()])
+
+    dispatch({
+      type: "INITIALIZE_CATALOG",
+      departments: data[0],
+      products: data[1]
+    })
+
+  } catch (error) {
+    dispatch({
+      type: "INITIALIZE_CATALOG",
+      departments: [],
+      products: [],
+    })
+  }
+}
+
 export const setFilter = department => {
   return {
     type: "SET_FILTER",
