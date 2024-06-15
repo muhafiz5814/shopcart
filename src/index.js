d71c0356a2633e561afcac107324a4fba07cbfe9
Muhafiz Raza <muhafizraza1209@gmail.com>
2024-06-15 11:41:44 +0530

    Setup and use redux-persist in App

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
+  <Provider store={store.store}>
+    <PersistGate loading={<h2>Loading...</h2>} persistor={store.persistor}>
+      <App />
+    </PersistGate>
   </Provider>, 
   document.getElementById("root")
 );
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,19 @@
 import { legacy_createStore as createStore, applyMiddleware } from "redux";
 import rootReducer from "./reducers";
 import {thunk} from "redux-thunk";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-export default createStore(rootReducer, applyMiddleware(thunk));
+const config = {
+  key: "shopcartCart",
+  storage,
+  whitelist: ["cart"]
+}
+
+const persistedReducer = persistReducer(config, rootReducer);
+const store = createStore(persistedReducer, applyMiddleware(thunk))
+
+export default {
+  store,
+  persistor: persistStore(store)
+}
